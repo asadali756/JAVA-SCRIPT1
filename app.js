609e6896a88ab37afcdb4666d8b4448ee89c5c92
asadali <asadaliabcd67@gmail.com>
2024-01-10 12:51:02 +0500

    JAVAS

---
+++ app.js
@@ -0,0 +1,105 @@
+let myfirstnumber = 10;
+console.log(myfirstnumber);
+let mysecondnumber = 20;
+console.log(mysecondnumber);
+let mythirdnumber = 30;
+console.log(mythirdnumber);
+let myfourthnumber = 40;
+console.log(myfourthnumber);
+let myfifthnumber = 20;
+console.log(myfifthnumber);
+
+
+// ADD VALUES
+
+let a = 15;
+let b = 12;
+let c = 12;
+let d = 20;
+let e = 29;
+let f = a + b + c + d + e;
+console.log(f);
+
+//SUBTRACT VALUES
+
+let g = 10;
+let h = 5;
+let i = g - h;
+console.log(i)
+
+
+//DATA TYPES
+
+let name1 = "ASAD"
+let name2 = "ALI"
+let merge = name1 + name2;
+console.log(merge)
+
+//OPERATORS
+
+let j = 22;
+let k = 30;
+console.log("THE SUM OF J & K",j+k);
+console.log("THE SUB OF J & K",j-k);
+console.log("THE DIVIDE OF J & k", j / k);
+console.log("THE MULTIPLY OF J & K ", j * k);
+
+//OPERATORTS
+
+let place ={
+    "FIRST": "PARK",
+    "SECOND": "APTECH",
+    "THIRD": "LUCKYONE",
+    "FOURTH" : "NATIONALSTADIUM",
+    "FIFTH": "KARACHIUNIVERSITY",
+}
+console.log(place)
+
+
+//ARRAYS
+
+let cricket = ["BABARAZAM", "VIRATKHOLI", "MRIZWAN", "BENSTOKES", "MAXWELL"];
+console.log(cricket);
+
+
+//LAST MEIN VALUE KO ADD KRNA HEY!
+
+console.log(cricket.push("WARNER"))
+console.log(cricket);
+
+//LAST SY VALUE KO REMOVE KRDEYGA!
+
+console.log(cricket.pop())
+console.log(cricket)
+
+// FIRST INDEX SY VALUE KO REMOVE KRDEYGA!
+
+console.log(cricket.shift())
+console.log(cricket)
+
+console.log(cricket.unshift("BABARAZAM"))
+console.log(cricket)
+
+//ONE BY ONE VALUE KO GET KR SAKHTEY HAIN!
+
+console.log(cricket[2])
+console.log(cricket[3])
+console.log(cricket.length)
+
+//LOGICAL OPERATORS & CASE!
+
+// console.log(true && true); //TRUE
+// console.log(true && false); //FALSE
+// console.log(false && true); //FALSE
+// console.log(false && false); //FALSE
+// console.log(true && true); //TRUE
+
+
+// OR CASE
+
+console.log(true || true); //TRUE
+console.log(true || false); //TRUE
+console.log(false || true); //TRUE
+console.log(false || false); //FALSE
+console.log(true || true); //TRUE
+
